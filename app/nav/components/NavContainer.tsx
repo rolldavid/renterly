d4f193e92fafac5fd29a4b98201a421f19f9ec45
rolldavid <rolldev@proton.me>
2022-11-28 16:00:52 -0600

    adding nav container

---
+++ app/nav/components/NavContainer.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import { useState, useEffect, useContext } from "react"
+import { ThemeContext } from "app/context/ThemeProvider";
+import Image from "next/image"
+import Link from "next/link"
+
+import NavLinks from "./NavLinks"
+import styles from "./NavContainer.module.css"
+import dark from "../assets/burger-dark.png"
+import light from "../assets/burger-light.png"
+import  NavModal from "./NavModal"
+
+export default function NavContainer() {
+    const [session, setSession] = useState<string>()
+    const theme = useContext(ThemeContext)
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        const getSession = async () => {
+            const res = await fetch("/api/get-session")
+            const data = await res.json();
+            setSession(data.email)
+            return;
+        }
+        getSession()
+    }, [])
+
+    useEffect(() => {
+        const savedTheme = window.localStorage.getItem("theme");
+        if (savedTheme) {
+            theme?.setTheme(savedTheme)
+        } else if (document.body.dataset.theme) {
+            theme?.setTheme(document.body.dataset.theme)
+            window.localStorage.setItem("theme", document.body.dataset.theme);
+        }
+        
+    }, []);
+
+    useEffect(() => {
+        document.body.dataset.theme = theme?.theme
+    }, [theme?.theme])
+
+    return (
+        <>
+            <div className={styles.desktop}>
+                <NavLinks isLoggedIn={session}/>
+            </div>
+            <div className={styles.mobile}>
+                <Link href={"/"} className={styles.logoContainer}>
+                    <h2 className={styles.logo}>ryo</h2>
+                </Link>
+               
+                <Image 
+                    src={theme?.theme === "light" ? dark : light} 
+                    alt={"Main menu hamburger"} 
+                    className={styles.burger}
+                    width={25} 
+                    height={25}
+                    aria-label={`Main Menu`}
+                    title={`Main Menu`}
+                    onClick={() => setShowModal(true)}
+                />
+            </div>
+            {showModal && <NavModal isLoggedIn={session} setShowModal={setShowModal}/>}
+        </>
+    )
+}
