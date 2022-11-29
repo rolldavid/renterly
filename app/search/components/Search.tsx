34a08a3b9e21b697c6c579d81f0c0aa55feccaa3
rolldavid <rolldev@proton.me>
2022-11-29 05:13:35 -0600

    init smarty

--- app/search/components/Search.tsx
+++ app/search/components/Search.tsx
@@ -1,11 +1,34 @@
 "use client"
 
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./Search.module.css"
+
 export default function Search() {
+   
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    const searchRef = useRef(null)
+
+    useEffect(() => {
+      const handleLookup = async () => {
+        const res = await fetch(`https://us-autocomplete-pro.api.smartystreets.com/lookup?key=${process.env.SMARTY_EMBEDDED_KEY}&search=${searchTerm}`)
+        const data = await res.json();
+        console.log(data)
+      }
+      handleLookup()
+    }, [searchTerm])
+
+
     return(
         <div>
-            <input className={styles.searchInput}/>
+            <input 
+                name="search"
+                value={searchTerm} 
+                className={styles.searchInput} 
+                onChange={e => setSearchTerm(e.target.value)}
+                ref={searchRef}
+            />
         </div>
         
     )
 }
+
