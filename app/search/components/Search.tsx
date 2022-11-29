54551d0733e3d8099e5599cd4413de1feba0908e
rolldavid <rolldev@proton.me>
2022-11-29 11:11:50 -0600

    expanded search

--- app/search/components/Search.tsx
+++ app/search/components/Search.tsx
@@ -3,7 +3,9 @@
 import React, { useState, useRef, useEffect } from "react";
 import { SearchProps } from "./types";
 import styles from "./Search.module.css"
-import SearchResults from "./SearchResults";
+import SearchItem from './SearchItem'
+
+let timeoutId: ReturnType<typeof setTimeout>;
 
 export default function Search() {
    
@@ -12,19 +14,30 @@ export default function Search() {
     const searchRef = useRef(null)
 
     useEffect(() => {
+      if (timeoutId) clearTimeout(timeoutId)
+    
       const handleLookup = async () => {
         if (!searchTerm) {
             setResults(null)
             return
         }
-        const res = await fetch(`https://us-autocomplete-pro.api.smartystreets.com/lookup?key=${process.env.NEXT_PUBLIC_SMARTY_KEY}&search=${searchTerm}`)
-        const data = await res.json();
-        setResults(data.suggestions)
-    
+        if (searchTerm.length >= 5) {
+            const res = await fetch(`https://us-autocomplete-pro.api.smartystreets.com/lookup?key=${process.env.NEXT_PUBLIC_SMARTY_KEY}&search=${searchTerm}&max_results=5`)
+            const data = await res.json();
+            setResults(data.suggestions)
+        }
       }
-      handleLookup()
+      timeoutId = setTimeout(handleLookup, 500)
+
     }, [searchTerm])
 
+    const expandSecondary = async (address: string, term: string) => {
+        console.log("...looking up secondary data...")
+        const res = await fetch(`https://us-autocomplete-pro.api.smartystreets.com/lookup?key=${process.env.NEXT_PUBLIC_SMARTY_KEY}&search=${term}&selected=${address}`)
+        const data = await res.json();
+        console.log("here are the extra results ========", data)
+        setResults(data.suggestions)
+      }
 
     return(
         <section className={styles.container}>
@@ -38,7 +51,11 @@ export default function Search() {
                 />
             </div>
             {results &&
-                    <SearchResults results={results}/>
+                    <div className={styles.resultsContainer}>
+                    {results.map((result, index) => {
+                        return <SearchItem key={index} result={result} expandSecondary={expandSecondary} searchTerm={searchTerm}/>
+                    })}
+                </div>
             }
         </section>
         
--- app/search/components/SearchItem.tsx
+++ app/search/components/SearchItem.tsx
@@ -1,13 +1,27 @@
 import styles from "./SearchItem.module.css"
 import { SearchProps } from "./types"
-export default function ResultItem({result}: {result: SearchProps}) {
+export default function ResultItem({result, expandSecondary, searchTerm}: {result: SearchProps, expandSecondary: (address: string, term: string) => {}, searchTerm: string}) {
+    if (result.secondary) {
+        
+        if (result.entries > 1) {
+            if (!result.secondary.includes("entries")) {
+            result.secondary += ` (${result.entries} entries)`
+            } 
+        } 
+      const pureSecondary = result.secondary.replace(`(${result.entries} entries)`, "").trim()
+      console.log("pure secondary", pureSecondary)
+      const address = `${result.street_line} ${pureSecondary} (${result.entries}) ${result.city} ${result.state} ${result.zipcode}`
+      return (
+        <div className={styles.container} onClick={() => expandSecondary(address, searchTerm)}>
+            {`${result.street_line} ${result.secondary} ${result.city} ${result.state} ${result.zipcode}`}
+        </div>
+      ) 
+    }
+
     return (
         <div className={styles.container}>
-            <p className={styles.resultItem}>{result.street_line}</p>
-            <p className={styles.resultItem}>{result.secondary}</p>
-            <p className={styles.resultItem}>{result.city}</p>
-            <p className={styles.resultItem}>{result.state}</p>
-            <p className={styles.resultItem}>{result.zipcode}</p> 
+            {`${result.street_line} ${result.city} ${result.state} ${result.zipcode}`}
         </div>
     )
 }
+
