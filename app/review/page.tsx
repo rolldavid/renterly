cb46fd6e9905b19f844a8103ce9588911a7aa8e7
rolldavid <rolldev@proton.me>
2022-12-08 14:46:11 -0600

    review page

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,27 +1,16 @@
-"use client"
 import { prisma } from "@/lib/prisma";
 import ReviewInput from "./components/ReviewInput";
-import { useQuery } from "@tanstack/react-query";
-import { getPropertySlug } from "@/lib/db-utils";
-import Spinner from "@/lib/utils/Spinner";
 
-export default function Page({searchParams} : {searchParams?: { [key: string]: string | undefined };}) {
-    const { data, status } = useQuery(["property"], () => {
-        if (searchParams && searchParams.slug) {
-            return getPropertySlug(searchParams.slug)
+
+export default async function Page({searchParams} : {searchParams?: { [key: string]: string | undefined };}) {
+
+    const property = await prisma.property.findUnique({
+        where: {
+            slug: searchParams?.slug
         }
     })
-
-    if (status === "loading") {
-        return <Spinner />
-    }
-
-    if (status === "success" && data) {
-        return (
-            <div>
-                <ReviewInput property={data.property}/>
-            </div>
-        )
+    if (property) {
+        return <ReviewInput property={property}/>
     }
    
     return null;
