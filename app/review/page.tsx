2b6d04be8fe536483b8c81ff24ae600133190f4b
rolldavid <rolldev@proton.me>
2022-12-08 15:03:35 -0600

    slug

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,12 +1,12 @@
+
 import { prisma } from "@/lib/prisma";
 import ReviewInput from "./components/ReviewInput";
 
-
 export default async function Page({searchParams} : {searchParams?: { [key: string]: string | undefined };}) {
-
+    const slugParam = searchParams?.slug ? searchParams.slug : "1212-1-2-12th-st-oroville-CA-95965"
     const property = await prisma.property.findUnique({
         where: {
-            slug: searchParams?.slug
+            slug: slugParam
         }
     })
     if (property) {
