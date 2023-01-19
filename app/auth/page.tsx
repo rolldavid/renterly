d8ef2d1966eded4b06268f24cb28ed6a05e63bd8
rolldavid <rolldev@proton.me>
2023-01-19 13:48:55 -0700

    auth page mobile

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,8 +1,36 @@
+"use client"
 
-export default async function Page() {
+import { useRouter } from "next/navigation";
+import { useQuery } from '@tanstack/react-query';
+import AuthContainer from "./components/AuthContainer"
+import styles from "@/styles/Auth.module.css"
+
+
+import { getUserSession } from '@/lib/db-utils';
+import Spinner from '@/lib/utils/Spinner';
+
+export default function Page() {
+  const router = useRouter();
+  const {data, status, isFetching, isRefetching} = useQuery(["session"], () => {
+        return getUserSession()
+  })
+
+  if (status === "loading" || isFetching || isRefetching) {
+    return <Spinner />
+  }
+
+  if (status === "success" && data.userId) {
+    router.push(`/account/${data.userId}`);
+  }
+
+  if (status === "success" && !data.userId) {
     return (
-        <div>
-            Auth
+        <div className={styles.container}>
+            <div className={styles.authModule}>
+                <AuthContainer fromReview={false}/>
+            </div>
         </div>
     )
+  }
+  return <Spinner />
 }
