7d4d8ebe522bac5e2b8f7aa0f8eac0b45c9a0fa7
rolldavid <rolldev@proton.me>
2023-04-30 06:05:45 -0700

    remove blog

--- app/blog/layout.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/styles/Blog.module.css"
-export default async function Layout({ children }: {
-    children: React.ReactNode;
-  }) {
- 
-    return (
-        <div className={styles.blogContainer}>{children}</div>
-    );
-  }
