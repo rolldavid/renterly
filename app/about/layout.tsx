2e1360c1d217ab08fde6ccaafa15d67e7a19c420
David <117313248+rolldavid@users.noreply.github.com>
2023-06-25 11:22:07 -0700

    Update layout.tsx

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -7,3 +7,34 @@ export default async function Layout({ children }: {
         <div className={styles.propertyWrapper}>{children}</div>
     );
   }
+
+export const metadata: Metadata = {
+    viewport: {
+      width: 'device-width',
+      initialScale: 1,
+      maximumScale: 1,
+    },
+    title: "About Us",
+    description: "Rate your landlord. Search and review any property.",
+    twitter: {
+      card: 'summary_large_image',
+      title: 'renterly',
+      description: 'Search and review any property.',
+      images: ['https://d3h42dhdxazsqn.cloudfront.net/renterlyblue.png'],
+    },
+    openGraph: {
+      title: "renterly",
+      description: "Search and review any property. A voice for every renter.",
+      url: `https://renterly.org/`,
+      images: [
+        {
+        url: `https://d3h42dhdxazsqn.cloudfront.net/renterlyblue.png`,
+        width: 1200,
+        height: 675,
+        alt: "renterly"
+        }
+      ],
+      type: "website",
+      locale: "en-US"
+    },
+  }  
